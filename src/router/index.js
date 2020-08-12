a5800cefc93a65312880e3991716cafaa34fc4fd
Marco Puccio <mpuccio90@gmail.com>
2020-08-11 21:43:59 -0300

    use hash router

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,28 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import Home from '@/views/Home';
-import AboutMe from '@/views/AboutMe';
-import Experience from '@/views/Experience';
+import Home from "@/views/Home";
+import AboutMe from "@/views/AboutMe";
+import Experience from "@/views/Experience";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
-  mode: 'history',
+  mode: "hash",
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home,
     },
     {
-      path: '/about-me',
-      name: 'aboutMe',
+      path: "/about-me",
+      name: "aboutMe",
       component: AboutMe,
     },
     {
-      path: '/experience',
-      name: 'experience',
+      path: "/experience",
+      name: "experience",
       component: Experience,
     },
   ],
